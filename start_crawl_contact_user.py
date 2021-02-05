50a944e6fe12ac1f0d419ee3fafac41db660121e
zestcode <58617234+zestcode@users.noreply.github.com>
2021-02-05 20:35:40 +0800

    Update start_crawl_contact_user.py

--- start_crawl_contact_user.py
+++ start_crawl_contact_user.py
@@ -3,7 +3,7 @@ import os
 import getopt
 from TelegramSpider.crawler import contact_user_crawler
 import settings
-
+#储存用户
 project_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_dir)
 
